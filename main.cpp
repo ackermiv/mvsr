7c4f2f55303c1e1eb1c24c67df3d65960081b979
ackermiv <ivo.ackermann@gmx.at>
2023-03-11 16:15:00 +0100

    cleanup and syntax improvements

--- main.cpp
+++ main.cpp
@@ -11,22 +11,17 @@ using namespace std;
 //Class template
 class discreteConvolution
 {   
-private:
-    //private members
+private: //private members
     cv::Mat kernel_;
 
-public:
-    //public members
-    
-    //default constructor
+public: //public members
+    //constructors
     discreteConvolution();
     discreteConvolution(cv::Mat);
-    //deconstructor
-    ~discreteConvolution();
     
     //public member functions
     void print();
-    cv::Mat conv(cv::Mat);
+    void conv(cv::Mat image, cv::Mat &convimage);
 };
 
 //default constructor
@@ -36,6 +31,7 @@ discreteConvolution::discreteConvolution()
     kernel_ = cv::Mat::zeros(3,3, CV_64F);
 }
 
+
 //angabe constructor
 discreteConvolution::discreteConvolution(cv::Mat kernel)
 {
@@ -43,24 +39,17 @@ discreteConvolution::discreteConvolution(cv::Mat kernel)
     kernel_ = kernel;
 }
 
-//deconstructor
-discreteConvolution::~discreteConvolution()
-{
-    //clean up any resources
-}
 
 //public member functions definitions
 void discreteConvolution::print()           //for debugging
 {
-    //function implementation
     std::cout << kernel_ << endl;
 }
 
-cv::Mat discreteConvolution::conv(cv::Mat image)
+void discreteConvolution::conv(cv::Mat image, cv::Mat &convimage)
 {
     image.convertTo(image, CV_64FC1);
     double min, max; 
-    cv::Mat convimage;
     convimage = cv::Mat::zeros(image.rows-kernel_.rows+1,image.cols-kernel_.cols+1, CV_64FC1);
     double sum = 0;
         for(int i = 0; i < image.rows-kernel_.rows+1; i++)
@@ -76,74 +65,68 @@ cv::Mat discreteConvolution::conv(cv::Mat image)
                     }
                 }
                 convimage.at<double>(i,j)=sum;
-                //min=std::min(min,sum);
-                //max=std::max(max,sum);
+
             }
         }
 
-    /*    cv::minMaxIdx (convimage, &min, &max,NULL);
-    cout  << "min " << min << "  max " << max <<endl;
-    convimage = convimage-min;
-    convimage = (convimage/(max-min))*255;
-    convimage.convertTo(convimage, CV_8UC1);
-    return convimage; */
-
-
-    //convimage = convimage-min;
-    //convimage = (convimage/(max-min))*255;
-    //convimage.convertTo(convimage, CV_8UC1);
-    return convimage;
 }
+
+
 
 class sobelDetector
 {
-private:
-        double m[3][3] = {{1., 2., 1.}, {0., 0., 0.}, {-1., -2., -1.}};     //explicit definition of double array
-        cv::Mat kernel_ = cv::Mat(3, 3, CV_64F, m);                         //feed Mat with the double array 
-        discreteConvolution sobelx_ = discreteConvolution(kernel_);
-        discreteConvolution sobely_ = discreteConvolution(kernel_.t());
+    private:
+            double m[3][3] = {{1., 2., 1.}, {0., 0., 0.}, {-1., -2., -1.}};     //explicit definition of sobel gradient detector kernel
+            cv::Mat kernel_ = cv::Mat(3, 3, CV_64F, m);                         //feed Mat with the double array 
+            discreteConvolution sobelx_ = discreteConvolution(kernel_);
+            discreteConvolution sobely_ = discreteConvolution(kernel_.t());     //.t transposes cv::Mat
 
-public:
-    sobelDetector(){}
-    
-    
-    void getEdges(cv::Mat &imgsobel, cv::Mat img) 
-    {
-        if (img.type()!=CV_64F) img.convertTo(img, CV_64F);                  //check if img is double and convert if it isnt
-        if (imgsobel.type()!=CV_64F) imgsobel.convertTo(imgsobel, CV_64F);   //check if img is double and convert if it isnt
+
+    public:
+        sobelDetector(){}
         
-        //sobelx_.print();
-        cv::Mat imgsobelx = sobelx_.conv(img);                          //sobel in one dimension
-        //sobely_.print();
-        cv::Mat imgsobely = sobely_.conv(img);                          //sobel in the other dimension
-        imgsobel = (imgsobelx.mul(imgsobelx)+imgsobely.mul(imgsobely)); //pythagoras in two lines
-        cv::sqrt(imgsobel,imgsobel);
-    }
+        //returns the edges of an image to imgsobel
+        void getEdges(cv::Mat img,cv::Mat &imgsobel) 
+        {
+            if (img.type()!=CV_64F) img.convertTo(img, CV_64F);                  //check if img is double and convert if it isnt
+            if (imgsobel.type()!=CV_64F) imgsobel.convertTo(imgsobel, CV_64F);   //check if img is double and convert if it isnt
+            
+            //sobelx_.print();
+            cv::Mat imgsobelx, imgsobely;
+            sobelx_.conv(img, imgsobelx);                          //sobel in one dimension
+            //sobely_.print();
+            sobely_.conv(img, imgsobely);                          //sobel in the other dimension
+            imgsobel = (imgsobelx.mul(imgsobelx)+imgsobely.mul(imgsobely)); //pythagoras in two lines
+            cv::sqrt(imgsobel,imgsobel);
+        }
 
 
 };
+
+
 
 //color segmentation of BGR based on the channel chosen in prefered_channel
 void segment(std::vector<cv::Mat> BGR, cv::Mat &color, int prefered_channel) 
 {
     if( prefered_channel < 0 || prefered_channel > 2 ) {
-        std::cout << "choose channel 0,1 or 2\n"; // Print error msg
+        std::cout << "choose channel 0,1 or 2\n";                           // Print error msg
         color=cv::Mat::zeros(3,3,CV_64F);                                  // Failure doesn't occur to lazy find a compilable solution
     }
     
-    for(int i=0; i< BGR.size() ;++i){                       //check if BGR is double and convert if it isnt
+    for(int i=0; i< BGR.size() ;++i){                                       //check if BGR is double and convert if it isnt
     if (BGR[i].type()!=CV_64F) BGR[i].convertTo(BGR[i], CV_64F);  
     }
+    if (color.type()!=CV_64F) color.convertTo(color, CV_64F);                 //check if img is double and convert if it isnt
 
     if(prefered_channel==0) color =   2*BGR[0] - BGR[1] - BGR[2];       //color segmentation based on chosen channel
     if(prefered_channel==1) color = - BGR[0] + 2*BGR[1] - BGR[2];
     if(prefered_channel==2) color = - BGR[0] - BGR[1] + 2*BGR[2];
-    
 }
 
 
+
 //returns an 8bit mat that is scaled to the [0,255] interval
-cv::Mat normalize(cv::Mat mat)
+void normalize(cv::Mat mat, cv::Mat &normmat)
 {
     if (mat.type()!=CV_64F) mat.convertTo(mat, CV_64F);                 //check if img is double and convert if it isnt
     
@@ -159,24 +142,17 @@ cv::Mat normalize(cv::Mat mat)
     mat = mat-min;
     mat = (mat/(max-min))*255;
     //cout  << "min " << min << "  max " << max <<endl;
-    mat.convertTo(mat, CV_8UC1);
-    return mat;
+    mat.convertTo(normmat, CV_8UC1);
+    
 }
 
-int main( int argc, char** argv ) {
-    if( argc != 2 ) {           //example code by woeber
+
+
+int main( int argc, char** argv ) {             //example code by woeber
+    if( argc != 2 ) {           
         std::cout << "Usage: ./OpenCV <Pfad-zum-Bild>\n"; // Pritn usage message
         return -1;                                        // Failure occurs
     }
-
-
-    double m[3][3] = {{1, 2, 1}, {2, 4, 2}, {1, 2, 1}};     //explicit definition of double array
-    cv::Mat kernel = cv::Mat(3, 3, CV_64F, m);              //feed Mat with the double array 
-    discreteConvolution *denoise = new discreteConvolution(kernel);
-    //denoise->print();
-
-
-
 
     std::string path_to_image = std::string( argv[ 1 ] );    //example code by woeber
     std::cout << "Got path to image:" << path_to_image << std::endl;
@@ -187,45 +163,35 @@ int main( int argc, char** argv ) {
     if( img.rows <= 0 ) {
         std::cout << "Faulty iamge file\n"; // Pritn error msg
         return -1;                          // Failure occurs
-    }//End check image
+    }//End check image                          //end of excample code
+
+
+    double m[3][3] = {{1., 2., 1.}, {2., 4., 2.}, {1., 2., 1.}};     //explicit definition of double array gaussian
+    cv::Mat kernel = cv::Mat(3, 3, CV_64F, m);              //feed Mat with the double array 
+    discreteConvolution *denoise = new discreteConvolution(kernel);
+    //denoise->print();
 
     std::vector< cv::Mat > layers;
-    cv::Mat convimage, charconvimage, segimg; 
-    cv::split(img, layers); // Split channles 
+    cv::Mat convimage, charconvimage, segimg, edges, charedges, thresholdedimg; 
+    cv::split(img, layers);         // Split channles into 3 cv::Mats instead of 1 cv::Mat that has vec3 entries
+    segment(layers, segimg ,2);     // colorsegmentation
 
-    segment(layers, segimg ,2);
-    //segimg=normalize(segimg);
-
-    //bimg = {onemat,zeromat,eyemat}; //fill with different pointers?
-    //segimg=cv::Mat::zeros(img.rows, img.cols,CV_64F);
-
-    /*for (int i=0;i<3;++i)
-    {
-        convimage = denoise->conv(layers[i]);
-        convimage.convertTo(bimg.at(i), CV_8UC1);
-
-        //convimage.convertTo(charconvimage), CV_8UC1);                     //why does it overwrite itself if i save it in between? does it save a pointer to charconvimage into bimg?
-        //bimg.at(i)=charconvimage;
-    } */
-    convimage = denoise->conv(segimg);
-    charconvimage=normalize(convimage);
+    denoise->conv(segimg, convimage);   //linear convolution with gaussian kernel
+    normalize(convimage, charconvimage); //just for controlimage 
     
-    cv::Mat edges; 
     
-    sobelDetector sbldt;        //initialzise sobel class
-    sbldt.getEdges(edges, convimage);
-    
-    cv::Mat charedges=normalize(edges);     //make it bite sized
-    cv::Mat thresholdedimg;                 
+    sobelDetector sbldt;                    //initialzise sobel class
+    sbldt.getEdges(convimage, edges);       //uses sobel detector
+    normalize(edges, charedges);            //make it byte sized
+
     cv::threshold(charedges, thresholdedimg, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);  //otsu threshold returns binary image
 
 
     //Detect circles in the image
     vector<cv::Vec3f> circles;
     cv::HoughCircles(thresholdedimg, circles, cv::HOUGH_GRADIENT, 2,
-                     thresholdedimg.rows / 8,  // min distance between circles
-                     100, 30, 20, 50 // last two parameters (min_radius & max_radius) are circle size in pixel
-                     // 
+                     thresholdedimg.rows / 8,       // min distance between circles
+                     100, 30, 20, 50                // last two parameters (min_radius & max_radius) are circle size in pixels
     );
 
     cv::Mat imgcopy=charedges.clone();
@@ -236,20 +202,9 @@ int main( int argc, char** argv ) {
         //std::cout << circles[i];
         cv::Point center = cv::Point(c[0], c[1]);
         int radius = c[2];
-        // circle center
-        //cv::circle(imgcopy, center, 1, 255, 3, cv::LINE_AA);
-        // circle outline
-        cv::circle(imgcopy, center, radius, 255, 3, cv::LINE_AA);
+        cv::circle(imgcopy, center, radius, 255, 3, cv::LINE_AA);   // circle outline
     }
 
-    //Show the result
-    cv::imshow("Hough Circles", img);
-    
-    //cv::Mat merged;
-    //cv::merge(bimg, merged);
-
-    //cv::namedWindow("denoised Image", 0);
-    //cv::imshow("denoised Image", merged);
     cv::namedWindow("original Image", 0); 
     cv::imshow("original Image", img);
 
